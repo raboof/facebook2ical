559007275a9cb70c191e562d3eaaf5cb8a4b1cdb
Norma Daley <normadaley644@gmail.com>
2017-02-18 13:18:33 -0500

    Add paging support

--- src/main/scala/FbEvents.scala
+++ src/main/scala/FbEvents.scala
@@ -6,17 +6,28 @@ import scalaj.http._
 
 object FbEvents {
   case class FbEvent(id: String, name: String, description: String, startTime: ZonedDateTime, endTime: Option[ZonedDateTime]);
-  case class Response(data: List[FbEvent]);
+  case class Cursors(before: String, after: String);
+  case class Paging(cursors: Cursors);
+  case class Response(data: List[FbEvent], paging: Option[Paging]);
 }
 
 trait FbEvents extends FbJsonMarshalling {
   import FbEvents._
 
-  def getEvents(token: String, pageId: String): List[FbEvent] = {
-    val url = s"https://graph.facebook.com/v2.7/$pageId/events?access_token=$token";
+  def getEvents(token: String, pageId: String, cursor: Option[String] = None): List[FbEvent] = {
+    val url : String = cursor match {
+      case None => s"https://graph.facebook.com/v2.7/$pageId/events?access_token=$token";
+      case Some(after) => s"https://graph.facebook.com/v2.7/$pageId/events?access_token=$token&after=$after";
+    }
     Http(url).asString match {
-      case HttpResponse(body, 200, _) => 
-        body.parseJson.convertTo[Response].data
+      case HttpResponse(body, 200, _) =>
+        val response : Response = body.parseJson.convertTo[Response];
+        val currentResults : List[FbEvent] = response.data;
+        val nextCursor: Option[String] = response.paging map { _.cursors.after }
+        nextCursor match {
+           case None => currentResults
+           case Some(cursor) => currentResults ::: getEvents(token, pageId, Some(cursor))
+        }
     };
   }
 }
@@ -31,6 +42,8 @@ trait FbJsonMarshalling extends SnakifiedSprayJsonSupport {
       case _ => deserializationError("Not a string: $json")
     }
   })
-  implicit val eventFormat = jsonFormat5(FbEvent)
-  implicit val responseFormat: JsonReader[Response] = jsonFormat1(Response)
+  implicit val eventFormat = jsonFormat5(FbEvent);
+  implicit val cursorsFormat = jsonFormat2(Cursors);
+  implicit val pagingFormat = jsonFormat1(FbEvents.Paging);
+  implicit val responseFormat: JsonReader[Response] = jsonFormat2(Response); 
 }
