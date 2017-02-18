fc4aebfe45c70cdba915aed66f4bb86c474f6da7
Norma Daley <normadaley644@gmail.com>
2017-02-18 14:28:18 -0500

    Add test

--- src/test/scala/FbEventsSpec.scala
+++ src/test/scala/FbEventsSpec.scala
@@ -11,10 +11,13 @@ class FbEventsSpec extends WordSpec with Matchers with FbJsonMarshalling {
   "The fb json reader" should {
     "successfully read the example json" in {
       val string = Source.fromInputStream(getClass.getResourceAsStream("/events.json")).mkString
-      val events = string.parseJson.convertTo[Response].data
+      val response = string.parseJson.convertTo[Response]
+      val events = response.data
       events.size should be(25)
       events(0).id should be("978483572260625")
       events(0).name should be("Originators en DJ Dab en DJ The Shake (Manchester)")
+
+      response.paging shouldEqual Some(Paging(Cursors("OTc4NDgzNTcyMjYwNjI1", "NDc0ODA0NjI2MDUxMjA1")))
     }
   }
 }
