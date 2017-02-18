9827f267167a903349b18a618c65f2772fefe8a6
Norma Daley <normadaley644@gmail.com>
2017-02-18 13:34:12 -0500

    Remove accidental space

--- src/main/scala/FbEvents.scala
+++ src/main/scala/FbEvents.scala
@@ -36,7 +36,7 @@ trait FbJsonMarshalling extends SnakifiedSprayJsonSupport {
   import FbEvents._
   import DefaultJsonProtocol._
 
-  implicit val dateTi meFormat = lift(new JsonReader[ZonedDateTime] {
+  implicit val dateTimeFormat = lift(new JsonReader[ZonedDateTime] {
     override def read(json: JsValue) = json match {
       case JsString(value) => ZonedDateTime.parse(value.replaceAll("00$", ":00"), DateTimeFormatter.ISO_OFFSET_DATE_TIME)
       case _ => deserializationError("Not a string: $json")
